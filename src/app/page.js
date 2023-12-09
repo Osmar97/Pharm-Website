8326981caca3073c141b4314e6eed2023661599f
Osmar97 <osmar.graca@icloud.com>
2023-12-09 12:56:53 +0000

    alteração

--- src/app/page.js
+++ src/app/page.js
@@ -41,7 +41,7 @@ export default function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8 sm:p-16 bg-gray-100 mt-20">
+    <main className="flex min-h-screen flex-col items-center justify-center p-8 sm:p-16 bg-gray-100 mt-20">
       <section>
         <div>
         <Slider/>
